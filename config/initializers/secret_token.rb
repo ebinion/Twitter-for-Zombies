d99df6b57acd8b1096fd5eec944fac2bd0b4390b
ebinion <ezekiel@gridshiftdesign.com>
2012-11-08 23:01:13 -0600

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SarahTest::Application.config.secret_token = '7477bd072fc0526bafd1a47fb1f8c2aa1ca444ba8f5a8440eeb2f23fa34146008530d794230288d468391b655be40f207e833f95e6e10cb6373d549fca48da99'
